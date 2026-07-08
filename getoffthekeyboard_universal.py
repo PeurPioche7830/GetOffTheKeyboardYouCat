0d139a4ba3a47352b5200c9c2eba8fec93547d4b
PeurPioche <115025589+PeurPioche7830@users.noreply.github.com>
2026-07-08 20:34:57 +0200

    Update getoffthekeyboard_universal.py
    Added color, more (and better) text, and universal screen covering (works correctly on all displays)

--- getoffthekeyboard_universal.py
+++ getoffthekeyboard_universal.py
@@ -1,16 +1,21 @@
 import tkinter as tk
 from tkinter import ttk
 
+# Default settings
+DarkBrown = "#17120c"
+Beige   = "#fef8ef"
+KindaBrown   = "#655552"
+
 def open_secondary_window():
     # Create cat protect window.
     secondary_window = tk.Toplevel()
     secondary_window.title("Secondary Window")
-    # Window size (i'll find a way to make this work on all monitor sizes later)
-    window_width = 1920
-    window_height = 1080
     # get the screen dimension
     screen_width = secondary_window.winfo_screenwidth()
     screen_height = secondary_window.winfo_screenheight()
+    # Window size (works on all screen sizes! at least in theory.)
+    window_width = screen_width
+    window_height = screen_height
     # find the center point
     center_x = int(screen_width/2 - window_width / 2)
     center_y = int(screen_height/2 - window_height / 2)
@@ -19,6 +24,11 @@ def open_secondary_window():
     secondary_window.resizable(False,False)
     secondary_window.attributes('-topmost', 1, '-alpha',0.9)
     secondary_window.overrideredirect(1)
+    secondary_window.configure(bg=KindaBrown)
+    # text
+    label2 = tk.Label(secondary_window, text='Computer is currently locked',bg='#655552',fg='white', font=('Rubik', 40))
+    label2.pack()
+    label2.place(relx=0.5, rely=0.4, anchor='center')
 
     # Create a button to close (destroy) this window.
     button_close = ttk.Button(
@@ -32,12 +42,16 @@ def open_secondary_window():
 # Create the main window.
 main_window = tk.Tk()
 main_window.geometry('400x200')
+# main window stuff
 main_window.title("Get off the Keyboard, you cat!")
 main_window.iconphoto(False, tk.PhotoImage(file='canvas.png'))
+main_window.configure(bg=Beige)
 main_window.resizable(False,False)
-label1 = tk.Label(main_window, text='Get off the keyboard, you cat!',bg='orange',fg='white' )
+# text
+label1 = tk.Label(main_window, text='Get off the keyboard, you cat!',bg='#fef8ef',fg='#17120c', font=('Rubik', 19))
 label1.pack()
-# Create the button to open the cat-proofing
+label1.place(relx=0.5, rely=0.1, anchor='center')
+# cat-proofing button
 button_open = ttk.Button(
     main_window,
     text="Lock this computer from feline activity",
